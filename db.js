5b1fc6e2b21d4fb6c777e7bb3446843899408b36
Bania2001 <rabiabania2812@gmail.com>
2023-11-24 18:56:18 +0100

    blue_print

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: add database url
+  '', //
   {
     dialect: 'postgres',
     dialectOptions: {
